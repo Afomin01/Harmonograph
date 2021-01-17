016b3528dea95821ce28a1031969331d9df97886
Afomin <284718@niuitmo.ru>
2021-01-17 21:03:55 +0300

    Added custom OpenGLWidget

---
+++ OpenGLCustomWidget.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <qopenglwidget.h>
+#include "HarmonographManager.h"
+#include<GL/glut.h>
+
+class OpenGLCustomWidget : public QOpenGLWidget {
+public:
+    QColor firstColor = Qt::blue;
+    QColor secondColor = Qt::red;
+    QColor backgroundColor = Qt::white;
+
+    int penWidth = 1;
+    float zoom = 125;
+    bool useTwoColors = true;
+
+    HarmonographManager* manager = nullptr;
+	
+    OpenGLCustomWidget(QWidget* parent = nullptr);
+    ~OpenGLCustomWidget();
+
+protected:
+    void initializeGL() override;
+    void resizeGL(int w, int h) override;
+    void paintGL() override;
+	
+};
+
