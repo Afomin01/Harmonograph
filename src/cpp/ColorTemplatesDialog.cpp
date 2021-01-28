10230319b4574206a954a80f2ae8c3cd0718f45f
Afomin <284718@niuitmo.ru>
2021-01-29 02:48:45 +0300

    Added License to all .h and .cpp files

--- src/cpp/ColorTemplatesDialog.cpp
+++ src/cpp/ColorTemplatesDialog.cpp
@@ -1,3 +1,23 @@
+/*
+ *   Copyright 2020-2021 Artem Bakin and Dmitriy Kuperstein
+ *
+ *   This file is part of Harmonograph.
+ *
+ *   Harmonograph is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Harmonograph is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Harmonograph.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
 #include "ColorTemplatesDialog.h"
 
 ColorTemplatesDialog::ColorTemplatesDialog(QWidget* parent) : QDialog(parent) {
